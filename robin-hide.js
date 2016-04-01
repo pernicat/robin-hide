abdf79c5e7cae713d80c2a2a7261d3aa18920461
Tony Pernicano <pernicat@gmail.com>
2016-04-01 19:45:11 -0400

    Fixed multiple delete button issue.

--- robin-hide.js
+++ robin-hide.js
@@ -14,35 +14,37 @@
     var shitlist = [];
     var filteredMessages = $('<div class="robin-chat--message-list"></div>');
     var robinMessages = $('#robinChatMessageList');
-    
+
     robinMessages.after(filteredMessages).hide();
-    
-    $("#robinChatMessageList").bind('DOMNodeInserted', function(){
-        
-        robinMessages.find('.robin-message').each(function() {
+
+    robinMessages.bind('DOMNodeInserted', function(){
+
+        robinMessages.find('.robin-message').detach().each(function() {
             var username = $(this).find('.robin--username').text().trim();
-            
+
             //console.log(username);
-            
+
             if (-1 === $.inArray(username, shitlist)) {
                 var del = $('<button>X</button>');
                 del.click(function() {
                     shitlist.push(username);
-                    
+
                     $('.my-user--' + username).hide();
                     console.log(username, 'added to shitlist');
                 })
                 .css('padding', 2)
                 .css('font-size', 'xx-small');
-                
+
                 $(this).addClass('my-user--' + username);
 
                 $(this).find('.robin--username').before(del);
-                
-                filteredMessages.append($(this).detach());
+
+                console.log('adding message for', username);
+
+                filteredMessages.append($(this));
             }
         });
-        
+
     });
 
 })();
