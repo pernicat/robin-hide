b6ada20c8ef871d9373a29b688cd8aeed598e5ad
Tony Pernicano <pernicat@gmail.com>
2016-04-01 18:40:35 -0400

    Added robin-hide.js

---
+++ robin-hide.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         robin-hide
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Filter Robin Spam
+// @author       Tony Pernicano <pernicat@gmail.com>
+// @match        https://www.reddit.com/robin/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var shitlist = [];
+    var filteredMessages = $('<div class="robin-chat--message-list"></div>');
+    var robinMessages = $('#robinChatMessageList');
+    
+    robinMessages.after(filteredMessages).hide();
+    
+    setInterval(function() {
+        
+        robinMessages.find('.robin-message').each(function() {
+            var username = $(this).find('.robin--username').text().trim();
+            
+            //console.log(username);
+            
+            if (-1 === $.inArray(username, shitlist)) {
+                var del = $('<button>X</button>');
+                del.click(function() {
+                    shitlist.push(username);
+                    
+                    $('.my-user--' + username).hide();
+                    console.log(username, 'added to shitlist');
+                })
+                .css('padding', 2)
+                .css('font-size', 'xx-small');
+                
+                $(this).addClass('my-user--' + username);
+
+                $(this).find('.robin--username').before(del);
+                
+                filteredMessages.append($(this).detach());
+            }
+        });
+        
+    }, 100);
+
+})();
